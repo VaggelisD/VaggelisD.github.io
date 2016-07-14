e4f8f9faa31903821c20fb3a34ad1f20a6b39aa7
VaggelisD <vagosgan13@gmail.com>
2016-07-14 17:45:28 +0300

    Sections, footer and fixes
    Title says it all

--- js/main.js
+++ js/main.js
@@ -1,18 +1,30 @@
-$(document).ready(function(){
-    
-    // The website loads fast enough, so the animation
-    // doesnt have time to play.
-    // Thats what the set timeout does
-    
-   setTimeout(function(){
-       // After the 2secs, fadeOut the loading animation
+$(document).ready(function () {
+    setTimeout(function () {
+        // After the 2secs, fadeOut the loading animation
         $(".loader").fadeOut("slow");
-       
-       // On click of the burger nav, expand it
-        $('.mobile-nav').on('click', function(){
-           $('.header ul').slideToggle(500, 'easeInOutCubic');
-        });
-    
-   }, 2000);    // parametre is ms of loading time 
-});
 
+
+        // On click of the burger nav, expand it
+
+        $('.mobile-nav').on('click', function () {
+            $('.header ul').slideToggle(500, 'easeInOutCubic');
+        });
+    });
+}, 2000); // parametre is ms of loading time 
+
+
+// Smoth scrolling on anchor elements.
+$(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
