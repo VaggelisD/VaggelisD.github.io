5a8b21c42b1a82a6d6e05417976303159afe8d38
VaggelisD <vagosgan13@gmail.com>
2016-07-18 13:33:10 +0300

    Commenting code, refactoring looks
    Refactored hamburger menu colors, button colors.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// When the document is ready, set a timeout for 2 seconds
+// and play the animation.
 $(document).ready(function () {
     setTimeout(function () {
         // After the 2secs, fadeOut the loading animation
@@ -5,15 +7,15 @@ $(document).ready(function () {
 
 
         // On click of the burger nav, expand it
-
         $('.mobile-nav').on('click', function () {
             $('.header ul').slideToggle(500, 'easeInOutCubic');
         });
-    }, 2000);
-}); // parametre is ms of loading time 
+    }, 2000); // parametre is ms of loading time 
+}); 
 
 
-// Smoth scrolling on anchor elements.
+// Smooth scrolling on anchor elements.
+// Copied code from SO FeelsGoodMan
 $(function () {
     $('a[href*="#"]:not([href="#"])').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -29,11 +31,14 @@ $(function () {
     });
 });
 
+// After the window is on the second section,
+// trigger the class 'full-navbar' to create the
+// full-fledged fixed navbar
 $(window).scroll(function() {
-    var height = $(window).scrollTop();
+    var height = $(window).scrollTop(); // Get height of window
     
-    var position = 600;
-    if(height > position) {
+//var position = 600;  <-- Ideal position
+    if(height > 600) {
         $(".navbar").addClass("full-navbar");
     }
     else{
